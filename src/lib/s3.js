3699ffd4de8cd803e9457327a93223b86c555321
mirnajafizade <mirnajafizade@mahsan.co>
2024-10-18 14:43:42 +0330

    final version 1.0.1.

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const s3Client = new S3Client({
     region: 'us-east-1',
